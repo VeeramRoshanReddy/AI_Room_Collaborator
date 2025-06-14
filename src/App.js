17ef728337c60bc6f43473a2e631a096cb3d17b0
VeeramRoshanReddy <157306684+VeeramRoshanReddy@users.noreply.github.com>
2025-06-15 02:28:19 +0530

    Fixing Personel Work 1

--- src/App.js
+++ src/App.js
@@ -127,11 +127,15 @@ function App() {
                 />
                 <MainContent isSidebarOpen={isSidebarOpen}>
                   <Routes>
+                    {/* Temporarily render PersonalWork directly for debugging */}
+                    <Route path="/personal-work" element={<PersonalWork />} />
+                    {/* Original routes commented out temporarily */}
+                    {/*
                     <Route path="/dashboard" element={<Home />} />
                     <Route path="/rooms" element={<Rooms />} />
-                    <Route path="/personal-work" element={<PersonalWork />} />
                     <Route path="/settings" element={<Settings />} />
                     <Route path="*" element={<Navigate to="/dashboard" replace />} />
+                    */}
                   </Routes>
                 </MainContent>
               </>
